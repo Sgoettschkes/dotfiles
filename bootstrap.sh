dc172f8c744f4065a7b52db5240db0abef46bc61
Sebastian Göttschkes <sebastian.goettschkes@googlemail.com>
2013-11-20 20:08:03 +0100

    bootstrap is now copying files instead of symlinking them

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,26 +1,43 @@
 #!/bin/bash
 
-create_link () {
-    if [ ! -f $2 ]; then
-        ln -s $1 $2
+copy () {
+    # Check if file exists; if so, backup it
+    if [ -f $2 ]; then
+        echo "File $2 exists; creating backup"
+        mkdir -p ~/.dotfiles/backup/
+        cp -f $2 ~/.dotfiles/backup/
+        rm -f $2
     fi
+    # Check if folder exists; if not, create it
+    DIR=`dirname $2`
+    if [ ! -d $DIR ]; then
+        echo "Folder $DIR does not exist; creating"
+        mkdir -p $DIR
+    fi
+    # Copy file
+    cp $1 $2
 }
 
 BASEPATH=`pwd`
 
 # bash
-create_link $BASEPATH/bash/bash_aliases ~/.bash_aliases
-create_link $BASEPATH/bash/bashrc ~/.bashrc
+copy $BASEPATH/bash/bash_aliases ~/.bash_aliases
+copy $BASEPATH/bash/bashrc ~/.bashrc
 
 # git
-create_link $BASEPATH/git/gitconfig ~/.gitconfig
-create_link $BASEPATH/git/gitignore ~/.gitignore
+copy $BASEPATH/git/gitconfig ~/.gitconfig
+copy $BASEPATH/git/gitignore ~/.gitignore
 
 # screen
-create_link $BASEPATH/screen/screenrc ~/.screenrc
+copy $BASEPATH/screen/screenrc ~/.screenrc
 
 # vim
-create_link $BASEPATH/vim/vimrc ~/.vimrc
+copy $BASEPATH/vim/vimrc ~/.vimrc
 
-#ghci
-create_link $BASEPATH/ghc/ghci.conf ~/.ghc/ghci.conf
+# ghci
+copy $BASEPATH/ghc/ghci.conf ~/.ghc/ghci.conf
+
+# bin
+copy $BASEPATH/bin/backup ~/bin/backup
+copy $BASEPATH/bin/changeTag ~/bin/changeTag
+chmod 744 ~/bin/*
