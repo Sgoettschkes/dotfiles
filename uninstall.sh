8d66aef3958953c94b45880c27e0058c78dfbb8f
Sebastian Göttschkes <sebastian.goettschkes@googlemail.com>
2014-11-04 22:35:37 +0100

    Adding stuff to make uninstall work better (not active yet)

--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,39 @@
 #!/bin/bash
+set -o nounset
+set -o errexit
 
-echo "--- STARTING ---"
+success () {
+    printf "\e[32m$1\e[39m\n"
+}
+
+warning () {
+    printf "\e[33m$1\e[39m\n"
+}
+
+error () {
+    printf "\e[31m$1\e[39m\n"
+}
+
+remove () {
+    Target=$1
+    if [ -z "$Target" ]; then
+        error "Target not given"
+        return
+    fi
+
+    if [ ! -e $Target ]; then
+        error "Target does not exist"
+        return
+    fi
+
+    if [ -e $Target ]; then
+        rm -rf $Target
+        success "Dir $Target removed"
+    else
+        rm -f $Target
+        success "File $Target removed"
+    fi
+}
 
 # bash
 rm -f ~/.aliases
@@ -26,7 +59,7 @@ rm -f ~/.git/git-prompt.sh
 
 # vim
 rm -f ~/.vimrc
-rm -f ~/.vim/bundle
+rm -rf ~/.vim/bundle
 rm -rf ~/.vim/backup
 rm -rf ~/.vim/temp
 
@@ -43,13 +76,15 @@ rm -f ~/bin/mon
 rm -f ~/bin/moveTmpMusic
 rm -f ~/bin/organizeImages
 
+# tmux
 rm -f ~/.tmux.conf
 
+# irssi
 rm -f ~/.irssi/config
 rm -f ~/.irssi/furry.theme
 
+# asunder
 rm -f ~/.asunder
 
+# apollo
 rm -f ~/.apollo
-
-echo "--- STOPPING ---"
