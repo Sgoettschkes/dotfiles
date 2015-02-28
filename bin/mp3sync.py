bbc9d2feb48d8d4e1f5e187f806f4be27f9faf38
Sebastian Göttschkes <sebastian.goettschkes@googlemail.com>
2015-02-28 12:52:29 +0100

    Implementing rsync into mp3sync.py and moving first bands over

--- bin/mp3sync.py
+++ bin/mp3sync.py
@@ -1,7 +1,29 @@
 #!/usr/bin/env python
 
+import os
+import subprocess
+
+artists = [
+        # Hardcore Punk
+        "59 Times the Pain",
+        "Bane",
+        "Barcode",
+        "Biohazard",
+        "Blood for Betrayal",
+        "Bloodshed Remains",
+        "Born from Pain",
+        "Boysetsfire",
+        "Cancer Bats",
+        "Cast Iron Hike",
+        "Choke X Chain"
+        ]
+
 def main():
-    print('This is mp3sync')
+    for artist in artists:
+        dir = "/home/sebastian/Music/Alben/" + artist
+        if not os.path.isdir(dir):
+            dir = "/home/sebastian/Music/Digital/" + artist
+        subprocess.check_call(["rsync","--archive","--recursive","--verbose",dir,"/media/sebastian/E0D6-C698/Music"])
 
 if __name__ == "__main__":
    main() 
