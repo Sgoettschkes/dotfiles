f05661b2078548b8c05ab2452804b48eae1a3602
Sebastian Göttschkes <sebastian.goettschkes@googlemail.com>
2021-05-05 18:57:17 +0200

    Add zsh config

---
+++ zsh/oh-my-zsh-custom/aliases.zsh
@@ -0,0 +1,33 @@
+# File managed by Sgoettschkes/dotfiles
+# Do not change
+
+# Dir navigation
+alias ..='cd ..'
+
+# Some ls aliases
+alias l='ls -CF'
+alias la='ls -A'
+alias lg='ls -alF | grep'
+alias ll='ls -alF'
+
+# Shortcuts
+alias d=docker
+alias dc=docker compose
+alias g=git
+
+# Little helpers
+alias cl=clear
+alias cle='clear && pwd && ls -AFG'
+
+# Advanced system tools
+alias df='df -Tha --total'
+alias du='du -ach | sort -h'
+alias free='free -mt'
+alias psx='ps aux | grep'
+alias lef='less +F'
+
+# Rest API stuff
+alias myip='curl http://ipecho.net/plain; echo'
+
+# git shortcuts
+alias glt='git l `git describe --tags --abbrev=0`..HEAD'
